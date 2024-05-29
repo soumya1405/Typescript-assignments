b56d95f7c0c14c7999bc8494f06674d8a385f974
katta soumya <kattasoumya2002@gmail.com>
2024-05-29 18:27:56 +0530

    delete files

--- Assignments2/string.js
+++
@@ -1,8 +0,0 @@
-// type T = "string";
-function getString(str) {
-    if (typeof (str) === "string") {
-        return "Yes";
-    }
-    return "No";
-}
-console.log(getString(10));
--- Assignments2/string.ts
+++
@@ -1,8 +0,0 @@
-// type T = "string";
-function getString<T>(str:T){
-        if(typeof(str)=== "string"){
-            return "Yes";
-        }
-        return "No";
-}
-console.log(getString(10));
